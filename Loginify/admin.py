4fd77c6e7f2f55834c22e96d894a5d3e132973ac
Ankit Modi <ankitmodi128@gmail.com>
2025-02-16 21:21:31 -0600

    models and admin

--- Loginify/admin.py
+++ Loginify/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserDetails
 
 # Register your models here.
+admin.site.register(UserDetails)
+
+'''@admin.register(UserDetails)
+class UserDetailsTable(admin.ModelAdmin):
+    list_display = ('Username', 'Email', 'Password')
+    search_fields = ('Username', 'Email')
+    list_per_page = 10'''
